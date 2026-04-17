1776f1ac208962c34bc51912ae10a7faa62db56a
jqp2401 <jqueper2401@g.educaand.es>
2026-04-17 17:07:27 +0200

    Clase Calculadora arreglo de dos bugs

--- src/ar/charlycimino/ejemplos/uncheckedexceptions/Calculadora.java
+++ src/ar/charlycimino/ejemplos/uncheckedexceptions/Calculadora.java
@@ -8,16 +8,19 @@ package ar.charlycimino.ejemplos.uncheckedexceptions;
  * Encontrá más código en mi repo de GitHub: https://github.com/CharlyCimino
  */
 public class Calculadora {
-    public int factorial (int num) {
+    public int factorial(int num) {
         if (num < 0) {
-            throw new RuntimeException("No se puede calcular el factorial de un número negativo");
+            throw new IllegalArgumentException("No se puede calcular el factorial de un número negativo");
         }
         if (num > 12) {
-            throw new RuntimeException("Por limitaciones técnicas, no se puede calcular el factorial de un entero mayor que 12");
+            throw new IllegalArgumentException(
+                    "Por limitaciones técnicas, no se puede calcular el factorial de un entero mayor que 12"
+            );
         }
+
         int fact = 1;
         for (int i = 2; i <= num; i++) {
-            fact = fact * i;
+            fact *= i;
         }
         return fact;
     }
